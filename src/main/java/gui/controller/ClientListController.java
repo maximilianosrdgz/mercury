01f13b315c00ddd1b7b65a5c7148881953f580be
RODRM733 <Maximiliano.X.Rodriguez.-ND@disney.com>
2017-09-18 13:30:42 -0300

    Client list update

--- src/main/java/gui/controller/ClientListController.java
+++ src/main/java/gui/controller/ClientListController.java
@@ -1,7 +1,15 @@
 package gui.controller;
 
+import controller.EntityManagerUtils;
+import dao.ClientDAO;
+import domain.Client;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import java.net.URL;
@@ -14,8 +22,23 @@ import java.util.ResourceBundle;
 @Controller
 public class ClientListController implements Initializable {
 
+    private ClientDAO clientDAO;
+    @FXML
+    private TableView<Client> tblClients;
+
+    @Autowired
+    public ClientListController(ClientDAO clientDAO) {
+        this.clientDAO = clientDAO;
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initClientsTable();
+    }
 
+    private void initClientsTable() {
+        ObservableList<Client> clients = FXCollections.observableArrayList();
+        clients.addAll(clientDAO.findAll());
+        tblClients.setItems(clients);
     }
 }
